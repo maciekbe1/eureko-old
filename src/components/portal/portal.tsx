8923c75583a0d6b19dd21883d75b93bf1350432e
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2021-10-23 22:59:50 +0200

    portal fix

--- src/components/portal/portal.tsx
+++ src/components/portal/portal.tsx
@@ -1,5 +1,5 @@
 import { createPortal } from "react-dom";
 
 export default function Portal({ children }: { children: React.ReactNode }) {
-  return createPortal(children, document.body);
+  return typeof window !== "undefined" && createPortal(children, document.body);
 }
